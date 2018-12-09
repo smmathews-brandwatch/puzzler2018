9e2686b4f7c46ebb2ad0a861e3860b68d58430a6
Shane Mathews <smathews@crimsonhexagon.com>
2018-12-09 17:28:09 -0500

    don't make the bot stay

--- baseBot.py
+++ baseBot.py
@@ -2,7 +2,7 @@ import botActions, time, random
 
 class BaseBot:
     def doAction(self):
-        actions = [botActions.sendStay,botActions.sendMoveDown, botActions.sendMoveUp, botActions.sendMoveLeft, botActions.sendMoveRight]
+        actions = [botActions.sendMoveDown, botActions.sendMoveUp, botActions.sendMoveLeft, botActions.sendMoveRight]
         action = actions[random.randint(0,len(actions)-1)]
         action()
         time.sleep(1) # sleep for a second so we can watch the dumb base bot
