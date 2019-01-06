cb7cc33aea868e8d62d437190c940ece5f695f49
Shane M Mathews <shane.m.mathews@gmail.com>
2019-01-05 22:16:11 -0500

    move torwards base, but prefre to pick up collectibles along the way

--- yourBot.py
+++ yourBot.py
@@ -1,27 +1,142 @@
-import botActions, time, random
+import botActions, time, random, queue, math
+from simulator import BoardPiece, Action, Position
+
+# soup nazi bot (no collectibles for you!)
+# 1) Attempt to deny the enemy bots their highest likelyhood collectibles right at the beginning
+# 2) Assume the bots are just as likely to attempt to move up, down, left, or right every frame.
+# 3) Based on bing, aykut, and damian's current sores (<9), don't worry too much about getting the 10th collectible.
+
+class Util:
+    def __init__(self, sim):
+        self.numCollectiblesOnPlayer = 0
+        self.numCollectiblesOnBotOneMoveFromBase = 0
+        self.playerId = None
+        self.enemyIds = []
+        for otherEntity in sim.board.entities:
+            if otherEntity.boardPiece == BoardPiece.Bot:
+                self.playerId = otherEntity.id
+            elif otherEntity.boardPiece == BoardPiece.Enemy:
+                self.enemyIds.append(otherEntity.id)
+        if otherEntity.ownerId == self.playerId:
+            self.numCollectiblesOnPlayer += 1
+        elif otherEntity.ownerId in self.enemyIds:
+            self.enemyIds.append(otherEntity.ownerId)
+
+class Node:
+    def __init__(self, position, g, h, parent, move, numCollectiblesOnPath, boardPiece):
+        self.position = position
+        self.g = g
+        self.h = h
+        self.f = g+h-(numCollectiblesOnPath*1.25)
+        self.parent = parent
+        self.move = move
+        self.numCollectiblesOnPath = numCollectiblesOnPath
+        self.boardPiece = boardPiece
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+# do an A* search to get from the start to the end
+class Path:
+    def __init__(self, sim, util, start, end):
+        open_list = []
+        closed_list = []
+        field = []
+        while len(field) < sim.board.height*sim.board.width:
+            field.append(BoardPiece.Empty)
+        for otherEntity in sim.board.entities:
+            if otherEntity.ownerId is None:
+                field[otherEntity.position.y*sim.board.width + otherEntity.position.x] = otherEntity.boardPiece
+        
+        start_node  = Node(start, 0, 0, None, None, 0, None)
+        end_node = Node(end, 0, 0, None, None, 0, None)
+        open_list.append(start_node)
+
+        while len(open_list) > 0:
+            # Get the current node
+            current_node = open_list[0]
+            current_index = 0
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+            
+            # Pop current off open list, add to closed list
+            open_list.pop(current_index)
+            closed_list.append(current_node)
+
+            # Found the goal
+            if current_node == end_node:
+                current = current_node
+                self.collectibles = current.numCollectiblesOnPath
+                while current.move is not None:
+                    self.move = current.move
+                    if current.boardPiece == BoardPiece.Collectible:
+                        self.collectibles += 1
+                    current = current.parent
+                return
+            
+            # generate the children
+            children = []
+            for moveTuple in [(Action.MoveUp,(0,-1)), (Action.MoveDown,(0,1)), (Action.MoveLeft,(-1,0)), (Action.MoveRight,(1,0))]:
+                newPosition = Position(x=current_node.position.x+moveTuple[1][0], y=current_node.position.y+moveTuple[1][1])
+                move = moveTuple[0]
+                if(newPosition.x >= 0 and newPosition.x < sim.board.width and newPosition.y >= 0 and newPosition.y < sim.board.height):
+                    piece = field[newPosition.y*sim.board.width + newPosition.x]
+                    g = current_node.g + 1
+                    h = math.sqrt(((newPosition.x - end_node.position.x) ** 2) + ((newPosition.y - end_node.position.y) ** 2))
+                    numCollectiblesOnPath = current_node.numCollectiblesOnPath
+                    if(piece == BoardPiece.Collectible and current_node.numCollectiblesOnPath < sim.maxCollectibles):
+                        numCollectiblesOnPath += 1
+                        children.append(Node(newPosition, g, h, current_node, move,numCollectiblesOnPath, piece))
+                    elif(newPosition == end or piece == BoardPiece.Empty):
+                        children.append(Node(newPosition, g, h, current_node, move, numCollectiblesOnPath, piece))
+
+            # Loop through children
+            for child in children:
+                # Child is on the closed list
+                for closed_child in closed_list:
+                    if child == closed_child:
+                        continue
+                # Child is already in the open list
+                for open_node in open_list:
+                    if child == open_node and child.g > open_node.g:
+                        continue
+
+                # Add the child to the open list
+                open_list.append(child)
+
+class PathToPlayerBase(Path):
+    def __init__(self, sim, util):
+        start = None
+        end = None
+        for otherEntity in sim.board.entities:
+            if util.playerId == otherEntity.id:
+                start = otherEntity.position
+            elif otherEntity.boardPiece == BoardPiece.BotBase:
+                end = otherEntity.position
+        super().__init__(sim, util, start, end)
 
 class YourBot:
     def doAction(self, botActionsWrapper):
-        # note a few things:
-        # 1) The entities will execute their actions in the order they're listed in the sim.board.entities list
-        # 2) The player's bot can't move on to a space occupied by another entity, except for a collectible the player is picking up
-        # 3) The player can't hold up more than 5 collectibles
-        # 4) When the player attempts to move on to their home base, they will instead deposit all their collectibles in their base
-        # 5) There is a limit to the number of frames in a round
-        # 6) There is a limit to the number of rounds before the final judgement
-        # 7) Your bot is judged on the avg, high, and low of the following:
-        #   a) Score. This is the number of collectibles you rescued minus the number of collectibles you lost.
-        #   b) Rescued. This is the number of collectibles you deposited in your base.
-        #   c) Lost. This is the number of collectibles your enemy deposited in their base.
-        # below are your available actions
-        #sim = botActionsWrapper.getSim()
-        #actionsThisRound = botActionsWrapper.sendMoveDown
-        #actionsThisRound = botActionsWrapper.sendMoveUp
-        #actionsThisRound = botActionsWrapper.sendMoveLeft
-        #actionsThisRound = botActionsWrapper.sendMoveRight
-        actionsThisRound = botActionsWrapper.sendStay()
-        print('implement me')
-        # remove this sleep to run as fast as you can
+        sim = botActionsWrapper.getSim()
+        if(sim != None):
+            util = Util(sim)
+            toBase = PathToPlayerBase(sim, util)
+            # toBase.collectibles == sim.maxCollectibles or toBase.collectibles == util.numCollectiblesLeft:
+            if True: 
+                if toBase.move == Action.MoveUp:
+                    actionsThisRound = botActionsWrapper.sendMoveUp()
+                elif  toBase.move == Action.MoveDown:
+                    actionsThisRound = botActionsWrapper.sendMoveDown()
+                elif  toBase.move == Action.MoveLeft:
+                    actionsThisRound = botActionsWrapper.sendMoveLeft()
+                elif  toBase.move == Action.MoveRight:
+                    actionsThisRound = botActionsWrapper.sendMoveRight()
+            # else if there is 1 or more collectibles to be picked up
+            #   move towards collectible closest to an enemy
+            #else:
+            #   start the next game
         time.sleep(1)
 
 if __name__ == "__main__":
